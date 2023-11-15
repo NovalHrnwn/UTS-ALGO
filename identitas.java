a4b38657926de9699b147f0f4019dd24fddb4671
NovalHrnwn <150902414+NovalHrnwn@users.noreply.github.com>
2023-11-15 19:44:08 +0700

    identitas.java

---
+++ identitas.java
@@ -0,0 +1,20 @@
+public class BIODATA {
+  
+       
+    public static void main(String[] args) {
+        // Mendefinisikan variabel dengan tipe data yang berbeda
+        String nama = "Noval Hernawan";  // String untuk nama
+        int usia = 19;  // Integer untuk usia
+        float tinggiBadan = 165.5f;  // Float untuk tinggi badan
+        String alamat = "Kp.Lebak Pasar RT 03/02";  // String untuk alamat
+        String nomorTelepon = "083899494038";  // String untuk nomor telepon
+
+        // Menampilkan biodata diri
+        System.out.println("Biodata Diri");
+        System.out.println("Nama: " + nama);
+        System.out.println("Usia: " + usia + " tahun");
+        System.out.println("Tinggi Badan: " + tinggiBadan + " cm");
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Nomor Telepon: " + nomorTelepon);
+    }
+}
